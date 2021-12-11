2104c681ac94fb936b8bf55ab2cc88dc0f44d673
Ray <89797110+Whatup364@users.noreply.github.com>
2021-12-11 17:25:44 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -49,7 +49,7 @@ export default class Command extends BaseCommand {
                 ]
                     .map((command) => command.config?.command)
                     .join(', ')}\`\`\`\n\n`
-            return void this.client.sendMessage(M.from, { url: rin }, MessageType.video, {quoted:M.WAMessage,
+            return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {quoted:M.WAMessage,
             mimetype: Mimetype.gif,
             caption: `${text} 
  ──❅┈[ ᖇᎥᑎ ᗷᗝ丅 ]┈❅───
