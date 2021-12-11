3e5e3c411e4b9b6698f4ca5a0c776df7a53628f8
Ray <89797110+Whatup364@users.noreply.github.com>
2021-12-11 17:21:55 +0530

    Update Wallpaper.ts

--- src/commands/Weeb/Wallpaper.ts
+++ src/commands/Weeb/Wallpaper.ts
@@ -1,12 +1,12 @@
 /** @format */
 
-import { AnimeWallpaper } from "anime-wallpapers";
+import { AnimeWallpaper } from "anime-wallpaper";
 import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
 import WAClient from "../../lib/WAClient";
 import { IParsedArgs, ISimplifiedMessage } from "../../typings";
+import request from "../../lib/request";
 import { MessageType } from "@adiwajshing/baileys";
-
 export default class Command extends BaseCommand {
 	constructor(client: WAClient, handler: MessageHandler) {
 		super(client, handler, {
