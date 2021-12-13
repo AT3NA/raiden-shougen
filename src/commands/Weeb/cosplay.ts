9becf6e6ea7c0c3431fdcf6a92fb27a9c619ec8f
Ray <89797110+Whatup364@users.noreply.github.com>
2021-12-13 23:09:04 +0530

    Update cosplay.ts

--- src/commands/Weeb/cosplay.ts
+++ src/commands/Weeb/cosplay.ts
@@ -10,42 +10,24 @@ import { MessageType } from '@adiwajshing/baileys'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'cosplay',
-            description: `will send you random cosplay girl image.`,
-            aliases: ['cosplay,cplay'],
+            command: 'crossplay',
+            description: `Will send you random crossplay img.`,
+            aliases: ['cp'],
             category: 'weeb',
-            usage: `${client.config.prefix}cosplay `,
+            usage: `${client.config.prefix}crossplay`,
             baseXp: 50
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        // fetch result of https://api.ichikaa.xyz/api/image/cosplay from the API using axios
-        const { data } = await axios.get('https://api.ichikaa.xyz/api/image/cosplay')
-        const buffer = await request.buffer(data.url).catch((e) => {
-            return void M.reply(e.message)
-        })
-        while (true) {
-            try {
-                M.reply(
-                    buffer || 'Could not fetch image. Please try again later',
-                    MessageType.image,
+        // fetch result of https://waifu.pics/api/sfw/waifu from the API using axios
+        return void M.reply( await request.buffer(`https://velgrynd.herokuapp.com/api/image/cosplay`),
+        MessageType.image,
                     undefined,
                     undefined,
-                    `*Sent image*\n`,
+                    `🌟 Here you go.\n`,
                     undefined
-                ).catch((e) => {
-                    console.log(`This Error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
-                    // console.log('Failed')
-                    M.reply(`Could not fetch image. Here's the URL: ${data.url}`)
-                })
-                break
-            } catch (e) {
-                // console.log('Failed2')
-                M.reply(`Could not fetch image. Here's the URL : ${data.url}`)
-                console.log(`This Error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
-            }
-        }
-        return void null
+                ).catch((reason: any) =>
+            M.reply(`✖ An error occurred. Please try again later.`))
     }
 }
