c40aa03f08a88a233f06e5d62aec8936889de5c4
Ray <89797110+Whatup364@users.noreply.github.com>
2021-12-15 00:38:33 +0530

    Create scubbus.ts

---
+++ src/commands/Nsfw/scubbus.ts
@@ -0,0 +1,32 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+import request from '../../lib/request'
+import { MessageType } from '@adiwajshing/baileys'
+// import { MessageType, Mimetype } from '@adiwajshing/baileys'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'succubus',
+            description: `Will send you random succubus girl img.`,
+            aliases: ['cp'],
+            category: 'nsfw',
+            usage: `${client.config.prefix}succubus,
+            baseXp: 50
+        })
+    }
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        // fetch result of https://api.ichikaa.xyz/api/image?q=succubus from the API using axios
+        return void M.reply( await request.buffer(`https://api.ichikaa.xyz/api/image?q=succubus`)
+        MessageType.image
+                    undefined,
+                    undefined,
+                    `🌟 Here you go.\n`,
+                    undefined
+                ).catch((reason: any) =>
+            M.reply(`✖ An error occurred. Please try again later.`))
+    }
+}
