11b389f8434fe6d340545290149d9a5fc730745c
Ray <89797110+Whatup364@users.noreply.github.com>
2021-12-15 10:57:38 +0530

    Update scubbus.ts

--- src/commands/Nsfw/scubbus.ts
+++ src/commands/Nsfw/scubbus.ts
@@ -6,25 +6,20 @@ import axios from 'axios'
 import request from '../../lib/request'
 import { MessageType } from '@adiwajshing/baileys'
 // import { MessageType, Mimetype } from '@adiwajshing/baileys'
-export default class Command extends BaseCommand {
-	constructor(client: WAClient, handler: MessageHandler) {
-		super(client, handler, {
-			command: "succubus",
-			description: `Know it yourself.`,
-			aliases: ["succubus"],
-			category: "nsfw",
-			usage: `${client.config.prefix}succubus`,
-			baseXp: 50,
-		});
-	}
 
-	run = async (M: ISimplifiedMessage): Promise<void> => {
-		let caption = "";
-		caption += `🌟`;
-		if (!(await this.client.getGroupData(M.from)).nsfw)
-			return void M.reply(
-				`Don't be a pervert, Baka! This is not an NSFW group.`
-			);
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'succubus',
+            description: `Will send you random succubus girl img.`,
+            aliases: ['succubus'],
+            category: 'nsfw',
+            usage: `${client.config.prefix}succubus`,
+            baseXp: 50
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
         // fetch result of https://api.ichikaa.xyz/api/image?q=succubus from the API using axios
         return void M.reply( await request.buffer(`https://api.ichikaa.xyz/api/image?q=succubus`),
         MessageType.image,
@@ -34,10 +29,5 @@ export default class Command extends BaseCommand {
                     undefined
                 ).catch((reason: any) =>
             M.reply(`✖ An error occurred. Please try again later.`))
-	);
     }
 }
-© 2021 GitHub, Inc.
-Terms
-Privacy
-Security
