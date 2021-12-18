697507a3f6e4219233bb100549b26821f0be1bf9
Ray <89797110+Whatup364@users.noreply.github.com>
2021-12-18 10:37:28 +0530

    Update mifi.ts

--- src/commands/Nsfw/mifi.ts
+++ src/commands/Nsfw/mifi.ts
@@ -10,46 +10,24 @@ import { MessageType } from '@adiwajshing/baileys'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'nsfwwaifu',
-            description: `Will send you random nsfw mifi image.`,
-            aliases: ['mifi'],
+            command: 'succubus',
+            description: `Will send you random succubus girl img.`,
+            aliases: ['succubus'],
             category: 'nsfw',
-            usage: `${client.config.prefix}mifi`,
+            usage: `${client.config.prefix}succubus`,
             baseXp: 50
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         // fetch result of https://api.ichikaa.xyz/api/randomimage/milf?apikey=Kuxw2RRu from the API using axios
-        const { data } = await axios.get('https://api.ichikaa.xyz/api/randomimage/milf?apikey=Kuxw2RRu')
-        if ( !(await this.client.getGroupData(M.from)).nsfw)
-            return void M.reply(
-                `Don't be a pervert, Baka! This is not an NSFW group.`
-            )
-        const buffer = await request.buffer(data.url).catch((e) => {
-            return void M.reply(e.message)
-        })
-        while (true) {
-            try {
-                M.reply(
-                    buffer || 'Could not fetch image. Please try again later',
-                    MessageType.image,
+        return void M.reply( await request.buffer(`https://api.ichikaa.xyz/api/randomimage/milf?apikey=Kuxw2RRu`),
+        MessageType.image,
                     undefined,
                     undefined,
-                    `*Ahh...*\n`,
+                    `🌟 Here you go.\n`,
                     undefined
-                ).catch((e) => {
-                    console.log(`This Error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
-                    // console.log('Failed')
-                    M.reply(`Could not fetch image. Here's the URL: ${data.url}`)
-                })
-                break
-            } catch (e) {
-                // console.log('Failed2')
-                M.reply(`Could not fetch image. Here's the URL : ${data.url}`)
-                console.log(`This Error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
-            }
-        }
-        return void null
+                ).catch((reason: any) =>
+            M.reply(`✖ An error occurred. Please try again later.`))
     }
 }
