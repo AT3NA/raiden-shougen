7631f2c9c61ac09dd90db8c60a1b53942255c858
Ray <89797110+Whatup364@users.noreply.github.com>
2021-12-23 12:05:16 +0530

    Update igdl.ts

--- src/commands/Utils/igdl.ts
+++ src/commands/Utils/igdl.ts
@@ -23,7 +23,7 @@ export default class Command extends BaseCommand {
         if (!joined) return void M.reply('Provide the keywords you wanna search, Baka!')
         const chitoge = joined.trim()
         console.log(chitoge)
-        const { data } = await axios.get(`https://api.zekais.com/igdl2?url=${chitoge}&apikey=YMiIh5TF`)
+        const { data } = await axios.get(`https://api-xcoders.xyz/api/download/ig?url=${chitoge}&apikey=MawfYEaFuf`)
         if ((data as { error: string }).error) return void (await M.reply('Sorry, couldn\'t find'))
         const buffer = await request.buffer(data.result[0].url).catch((e) => {
             return void M.reply(e.message)
