cfe7b21caf6ac8ef0f4780a91e305683e7cbbe4e
Ray <89797110+Whatup364@users.noreply.github.com>
2022-01-10 19:51:49 +0530

    Update scubbus.ts

--- src/commands/Nsfw/scubbus.ts
+++ src/commands/Nsfw/scubbus.ts
@@ -25,7 +25,7 @@ export default class Command extends BaseCommand {
         `Don't be a pervert, Baka! This is not an NSFW group.`
          );
         // fetch result of https://velgrynd.herokuapp.com/api/randomimage?q=succubus&apikey=Kuxw2RRu from the API using axios
-        return void M.reply( await request.buffer(`https://velgrynd.herokuapp.com/api/randomimage?q=succubus&apikey=Kuxw2RRu`),
+        return void M.reply( await request.buffer(`https://velgrynd.herokuapp.com/api/randomimage?q=succubus&apikey=jxhcCGrCtIavLMAe6JY8xrwTX`),
         MessageType.image,
                     undefined,
                     undefined,
