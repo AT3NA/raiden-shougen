931efa2e0024c0f1a4db8bb0858718c07cea237d
Ray <89797110+Whatup364@users.noreply.github.com>
2022-01-12 09:57:33 +0530

    Update Rules.ts

--- src/commands/General/Rules.ts
+++ src/commands/General/Rules.ts
@@ -1,26 +1,35 @@
-import { MessageType, Mimetype } from '@adiwajshing/baileys'
-import { join } from 'path'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+import request from '../../lib/request'
+import { MessageType } from '@adiwajshing/baileys'
+// import { MessageType, Mimetype } from '@adiwajshing/baileys'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'rules',
+            description: `Get rules list`,
+            aliases: ['rules'],
+            category: 'general',
+            usage: `${client.config.prefix}rules`,
+            baseXp: 50
         })
     }
 
-    run = async (M: ISimplifiedMessage): Promise<void> => {
-        const n = [
+    run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
+            const n = [
             './assets/Rin/rules.mp4'
         ]
-        let chitoge = n[Math.floor(Math.random() * n.length)]
-        return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
-            mimetype: Mimetype.gif,
-            caption: `_*----🎀[Rule]🎀----*_\n\n❌*DONT ASK FOR THE SCRIPT*🚫\n*>>>* use ${this.client.config.prefix}support to get the Official group link in your dm\n*--->*  If you want to chat with Star you can use *Don't chat in private bot can't chat in Pm\n*--->* If you want to add Yotsuba in your group the contact the owner by *${this.client.config.prefix}owner/${this.client.config.prefix}mods* \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, its means the bot maybe offline or facing internet issue. \n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫  `,
-
-        )
+        MessageType.video,
+        mimetype: Mimetype.gif,
+                    undefined,
+                    undefined,
+                    `_*----🎀[Rule]🎀----*_\n\n❌*DONT ASK FOR THE SCRIPT*🚫\n*>>>* use $support to get the Official group link in your dm\n*--->*  If you want to chat with Star you can use *Don't chat in private bot can't chat in Pm\n*--->* If you want to add Yotsuba in your group the contact the owner by *$owner/$mods* \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, its means the bot maybe offline or facing internet issue. \n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫  `,
+                    undefined
+                ).catch((reason: any) =>
+            M.reply(`✖ An error occurred. Please try again later.`))
     }
 }
