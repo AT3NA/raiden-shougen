116e5d463af270b859644337f804aebbe0e81333
Ray <89797110+Whatup364@users.noreply.github.com>
2022-01-13 23:15:52 +0530

    Update caraamazon.ts

--- src/commands/Utils/caraamazon.ts
+++ src/commands/Utils/caraamazon.ts
@@ -1,11 +1,11 @@
+@@ -0,0 +1,53 @@
+import { MessageType } from '@adiwajshing/baileys'
+import request from '../../lib/request'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
-import { ISimplifiedMessage } from '../../typings'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
 import axios from 'axios'
-import request from '../../lib/request'
-import { MessageType } from '@adiwajshing/baileys'
-// import { MessageType, Mimetype } from '@adiwajshing/baileys'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
