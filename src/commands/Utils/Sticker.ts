8f950377a19e7fe3ef60003009964553bed3aabb
Calvy <90485412+ugsf47@users.noreply.github.com>
2022-01-22 22:36:36 +0200

    2.0.0

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -80,8 +80,8 @@ export default class Command extends BaseCommand {
 					})();
 					return {
 						categories,
-						pack: pack[1] || "🌟 Here you go ",
-						author: pack[2] || "Chitoge 🌟",
+						pack: pack[1] || "⭐ Here you go ",
+						author: pack[2] || "BeckyLynch ⭐",
 						quality,
 						type: StickerTypes[
 							parsedArgs.flags.includes("--crop") ||
