9487dac37a072b4cd64eb253483fd30f14a7f29b
Ray <89797110+Whatup364@users.noreply.github.com>
2022-02-04 11:31:17 +0530

    Update cosplay.ts

--- src/commands/Weeb/cosplay.ts
+++ src/commands/Weeb/cosplay.ts
@@ -21,7 +21,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         // fetch result of https://velgrynd.herokuapp.com/api/randomimage/cosplay?apikey=jxhcCGrCtIavLMAe6JY8xrwTX from the API using axios
-        return void M.reply( await request.buffer(`https://velgrynd.herokuapp.com/api/randomimage/cosplay?apikey=jxhcCGrCtIavLMAe6JY8xrwTX`),
+        return void M.reply( await request.buffer(`https://hanzz-web.herokuapp.com/api/randomimage/cosplay`),
         MessageType.image,
                     undefined,
                     undefined,
