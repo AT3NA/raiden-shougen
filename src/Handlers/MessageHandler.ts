8fd6c413c328dea88eccacce04c778c3c5f36a68
Ray <89797110+Whatup364@users.noreply.github.com>
2022-02-11 19:01:30 +0530

    Update MessageHandler.ts

--- src/Handlers/MessageHandler.ts
+++ src/Handlers/MessageHandler.ts
@@ -1,7 +1,9 @@
+import { MessageType } from '@adiwajshing/baileys/lib/WAConnection'
 import axios from 'axios'
 import chalk from 'chalk'
 import { join } from 'path'
 import BaseCommand from '../lib/BaseCommand'
+import request from '../lib/request'
 import WAClient from '../lib/WAClient'
 import { ICommand, IParsedArgs, ISimplifiedMessage } from "../typings";
 
@@ -91,9 +93,13 @@ export default class MessageHandler {
 			)}`
 		);
 			if (!command)
-				return void M.reply(
-					`No such command, Baka! Have you never seen someone use the command *${this.client.config.prefix}help*.`
-				);
+				return void M.reply( await request.buffer(`https://lh3.googleusercontent.com/pw/AM-JKLXDWWrhHtuvfs9E-inra71tvu8fsRB2LYhFcnn57Jr49Ja0T-24jo9nCpRu8uq5goCiSum2m33vmNczqCsyuCfc_5rY1wjL_LV8Tmer0q3D4Zs-pfJDmvvn2_R0iETsdl5D7iVx-JALFur3riS8xoH_=w1440-h810-no?authuser=0`),
+        MessageType.image,
+                    undefined,
+                    undefined,
+                    `No such command, Baka! Have you never seen someone use the command *${this.client.config.prefix}help*`,
+                    undefined
+                )
 		const user = await this.client.getUser(M.sender.jid);
 		if (user.ban) return void M.reply("You're Banned from using commands.");
 		const state = await this.client.DB.disabledcommands.findOne({
