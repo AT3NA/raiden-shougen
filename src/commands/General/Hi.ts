ebd9de5e4a5752945f51926b1a8945abbe19f4ec
Ronen6999 <101617329+Ronen6999@users.noreply.github.com>
2022-03-18 09:30:28 +0530

    Update Hi.ts

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const chitoge =
-			"https://c.tenor.com/NvYI5wQNfgUAAAPo/nakano-yotsuba-pout.mp4";
+			"https://c.tenor.com/ldlrOa43OlMAAAPo/majo-no-tabitabi-the-journey-of-elaina.mp4";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: chitoge },
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `I don't have time to have a conversation with someone like you. Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
+				caption: `Hello there, Elaina Here 💕. Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
 			}
 		);
 	};
