a67611ed5cf07d5ccea4e68b269ce7814f9e6cc0
Ronen6999 <101617329+Ronen6999@users.noreply.github.com>
2022-03-18 09:47:30 +0530

    Update Broadcast.ts

--- src/commands/Dev/Broadcast.ts
+++ src/commands/Dev/Broadcast.ts
@@ -29,13 +29,13 @@ export default class Command extends BaseCommand {
 			return void (await M.reply(`Please provide the Broadcast Message.`));
 		const term = joined.trim();
 		const gifs = [
-			"https://c.tenor.com/qGBj-9nx7rsAAAPo/what-the-quintessential-quintuplets.mp4",
-			"https://c.tenor.com/mMRNByRo8XMAAAPo/nakano-yotsuba-disturbance.mp4",
-			"https://c.tenor.com/yQ_zLRi6zUkAAAPo/yotsuba-nakano.mp4",
-			"https://c.tenor.com/DpLoI6rRq4YAAAPo/nakano-yotsuba-ribbon.mp4",
-			"https://c.tenor.com/dEnq15fNv6kAAAPo/aaaa-the-quintessential-quintuplets.mp4",
-			"https://c.tenor.com/i1fRMMMvu38AAAPo/yotsuba-nakano-the-quintessential-quintuplets.mp4",
-			"https://c.tenor.com/l2F9LrkJEKYAAAPo/yotsuba-nakano.mp4",
+			"https://c.tenor.com/oSQvUimhICgAAAPo/majo-no-tabitabi-the-journey-of-elaina.mp4",
+			"https://c.tenor.com/FTryuAk9O1sAAAPo/majo-no-tabitabi-the-journey-of-elaina.mp4",
+			"https://c.tenor.com/OdR8EigUHfoAAAPo/elaina-mage.mp4",
+			"https://c.tenor.com/S_fOSRz1ngcAAAPo/majo-no-tabitabi-the-journey-of-elaina.mp4",
+			"https://c.tenor.com/uG_QJUDWKm0AAAPo/majo-no-tabitabi-the-journey-of-elaina.mp4",
+			"https://c.tenor.com/rRaAZM3KYqcAAAPo/majo-no-tabitabi-the-journey-of-elaina.mp4",
+			"https://c.tenor.com/36P-Xu4XxRMAAAPo/elaina-anime.mp4",
 		];
 		const selected = gifs[Math.floor(Math.random() * gifs.length)];
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -46,7 +46,7 @@ export default class Command extends BaseCommand {
 			.map((jids) => (jids.includes("g.us") ? jids : null))
 			.filter((v) => v);
 		for (let i = 0; i < chats.length; i++) {
-			const text = `*⚡「YOTSUBA BROADCAST」⚡*\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
+			const text = `*💕「ELAINA BROADCAST」💕*\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
 			this.client.sendMessage(chats[i], { url: selected }, MessageType.video, {
 				mimetype: Mimetype.gif,
 				caption: `${text}`,
