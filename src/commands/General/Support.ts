184c7c895149d174a87dee2a84252b771cb5b134
Ronen6999 <101617329+Ronen6999@users.noreply.github.com>
2022-03-18 09:42:09 +0530

    Update Support.ts

--- src/commands/General/Support.ts
+++ src/commands/General/Support.ts
@@ -19,11 +19,11 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         (await this.client.sendMessage(
         M.sender.jid,
-        `    ♥️MY MASTER GROUP♥️\n\n*🎇YOTSUBA support:🎇* *https://chat.whatsapp.com/JkxmxZ593N16JYaiJ4Sx09*\n\n*💫INFINITY BOT FUN GROUP💫*:*https://chat.whatsapp.com/F0lsoalrK7yInLXShKdWJ6*`,
+        `    ♥️MY MASTER GROUP♥️\n\n*🎇ELAINA support:🎇* *https://chat.whatsapp.com/LWe4ldZ4HjIKWJlRhT6qRJ*\n\n*💫INFINITY BOT FUN GROUP💫*:*https://chat.whatsapp.com/Hooxu1kZEoFKoNpdFLqXMO*`,
            MessageType.text
         ))
         const n = [
-            'https://c.tenor.com/DnRViqnLNsUAAAPo/gotoubun-no-hanayome-anime.mp4'
+            'https://c.tenor.com/oSQvUimhICgAAAPo/majo-no-tabitabi-the-journey-of-elaina.mp4'
         ]
         let beckylynch = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url:beckylynch }, MessageType.video, {quoted:M.WAMessage,
