c2bc768107e5e2f771e21db642ec310bee44fb25
HARAJIT <94275149+AT3NA@users.noreply.github.com>
2022-03-19 20:46:11 +0530

    Update Hi.ts

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const chitoge =
-			"https://c.tenor.com/ldlrOa43OlMAAAPo/majo-no-tabitabi-the-journey-of-elaina.mp4";
+			"https://c.tenor.com/Fjp3Whgn_UYAAAPo/mikasa-ackerman-aot.mp4";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: chitoge },
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `Hello there, Elaina Here 💕. Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
+				caption: `Hello there, Mikasa Here 💕. Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
 			}
 		);
 	};
