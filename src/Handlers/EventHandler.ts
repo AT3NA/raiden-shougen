3dc3a19f4e1328a19558ea69ae3bc4d8969505cc
Ronen6999 <101617329+Ronen6999@users.noreply.github.com>
2022-03-19 09:10:58 +0530

    Update EventHandler.ts

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -19,7 +19,7 @@ export default class EventHandler {
         const text = add
 					? `- ${group.subject || "___"} -\n\n💠 *Group Description:*\n${
 							group.desc
-					  }\n\nHope you follow the rules and have fun!\n\n${event.participants
+					  }\n\nHope you follow the rules and have fun! If you want to add me in your grp than plz ask the bots mod use the command .mod\n\n${event.participants
 							.map((jid) => `@${jid.split("@")[0]}`)
 							.join(", ")}`
 					: event.action === "remove"
