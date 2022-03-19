3a93394729850cf0a35421705e5a1aecb21feded
HARAJIT <94275149+AT3NA@users.noreply.github.com>
2022-03-19 21:05:30 +0530

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -24,7 +24,7 @@ export default class Command extends BaseCommand {
                     user.notify || user.vname || user.name || 'null'
                 }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
-        text += `\n💕 Elaina 💕 `
+        text += `\n💕 MIKASA ACKERMAN ☯️ 💕 `
         return void M.reply(text)
     }
 }
