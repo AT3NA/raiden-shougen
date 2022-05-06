fbba532d1a11f9e1a961328aadb41236728a2ed2
HARAJIT <94275149+AT3NA@users.noreply.github.com>
2022-05-06 08:17:51 +0530

    Update Rules.ts

--- src/commands/General/Rules.ts
+++ src/commands/General/Rules.ts
@@ -14,12 +14,12 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            'https://c.tenor.com/-HVRXxjXP1YAAAPo/majo-no-tabitabi-the-journey-of-elaina.mp4'
+            'https://ahad-cloud.mirror-index.workers.dev/0:/Uploads/video_2022-05-06_02-46-57_7094449484353338368.mp4'
         ]
         let chitoge = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
             mimetype: Mimetype.gif,
-            caption: `_*----🎀[Rule]🎀----*_\n\n❌ *DONT ASK FOR THE SCRIPT* 🚫\n*>>>* use .support to get the Official group link in your dm\n*--->Don't chat in private bot can't chat in Pm\n*--->* If you want to add Elaina in your group the contact the owner by *.owner/.mods* \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, its means the bot maybe offline or facing internet issue. \n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫  ` }
+            caption: `_*----🎀[Rule]🎀----*_\n\n❌ *DONT ASK FOR THE SCRIPT* 🚫\n*>\n*--->Don't chat in private bot can't chat in Pm\n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, its means the bot maybe offline or facing internet issue. \n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫  ` }
         )
     }
 }
