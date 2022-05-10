2cd5e1ed9d2466e5f4b893e47fed786a556195d5
HARAJIT <94275149+AT3NA@users.noreply.github.com>
2022-05-10 06:43:01 +0530

    Update Broadcast.ts

--- src/commands/Dev/Broadcast.ts
+++ src/commands/Dev/Broadcast.ts
@@ -29,13 +29,13 @@ export default class Command extends BaseCommand {
 			return void (await M.reply(`Please provide the Broadcast Message.`));
 		const term = joined.trim();
 		const gifs = [
-			"https://c.tenor.com/oSQvUimhICgAAAPo/majo-no-tabitabi-the-journey-of-elaina.mp4",
-			"https://c.tenor.com/FTryuAk9O1sAAAPo/majo-no-tabitabi-the-journey-of-elaina.mp4",
-			"https://c.tenor.com/OdR8EigUHfoAAAPo/elaina-mage.mp4",
-			"https://c.tenor.com/S_fOSRz1ngcAAAPo/majo-no-tabitabi-the-journey-of-elaina.mp4",
-			"https://c.tenor.com/uG_QJUDWKm0AAAPo/majo-no-tabitabi-the-journey-of-elaina.mp4",
-			"https://c.tenor.com/rRaAZM3KYqcAAAPo/majo-no-tabitabi-the-journey-of-elaina.mp4",
-			"https://c.tenor.com/36P-Xu4XxRMAAAPo/elaina-anime.mp4",
+			"https://ahad-cloud.mirror-index.workers.dev/0:/Uploads/video_2022-05-06_02-12-59_7094440655376994304.mp4",
+			"https://ahad-cloud.mirror-index.workers.dev/0:/Uploads/video_2022-05-06_02-21-06_7094442746977236992.mp4",
+			"https://ahad-cloud.mirror-index.workers.dev/0:/Uploads/video_2022-05-06_02-35-40_7094446503847251968.mp4",
+			"https://ahad-cloud.mirror-index.workers.dev/0:/Uploads/video_2022-05-06_02-28-23_7094444621712323584.mp4",
+			"https://ahad-cloud.mirror-index.workers.dev/0:/Uploads/video_2022-05-06_02-46-57_7094449484353338368.mp4",
+			"https://ahad-cloud.mirror-index.workers.dev/0:/Uploads/video_2022-05-10_01-10-22_7095908865759848448.mp4",
+			"https://ahad-cloud.mirror-index.workers.dev/0:/Uploads/video_2022-05-10_01-11-32_7095909162462006272.mp4",
 		];
 		const selected = gifs[Math.floor(Math.random() * gifs.length)];
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -46,7 +46,7 @@ export default class Command extends BaseCommand {
 			.map((jids) => (jids.includes("g.us") ? jids : null))
 			.filter((v) => v);
 		for (let i = 0; i < chats.length; i++) {
-			const text = `*💕「ELAINA BROADCAST」💕*\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
+			const text = `*📢[LELOUCH BROADCAST」📢*\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
 			this.client.sendMessage(chats[i], { url: selected }, MessageType.video, {
 				mimetype: Mimetype.gif,
 				caption: `${text}`,
