625d112e49e4b267757b41219b96dd183b3d1cec
HARAJIT <94275149+AT3NA@users.noreply.github.com>
2022-05-11 16:54:41 +0530

    Update Broadcast.ts

--- src/commands/Dev/Broadcast.ts
+++ src/commands/Dev/Broadcast.ts
@@ -29,13 +29,13 @@ export default class Command extends BaseCommand {
 			return void (await M.reply(`Please provide the Broadcast Message.`));
 		const term = joined.trim();
 		const gifs = [
-			"https://ahad-cloud.mirror-index.workers.dev/0:/Uploads/video_2022-05-06_02-12-59_7094440655376994304.mp4",
-			"https://ahad-cloud.mirror-index.workers.dev/0:/Uploads/video_2022-05-06_02-21-06_7094442746977236992.mp4",
-			"https://ahad-cloud.mirror-index.workers.dev/0:/Uploads/video_2022-05-06_02-35-40_7094446503847251968.mp4",
-			"https://ahad-cloud.mirror-index.workers.dev/0:/Uploads/video_2022-05-06_02-28-23_7094444621712323584.mp4",
-			"https://ahad-cloud.mirror-index.workers.dev/0:/Uploads/video_2022-05-06_02-46-57_7094449484353338368.mp4",
-			"https://ahad-cloud.mirror-index.workers.dev/0:/Uploads/video_2022-05-10_01-10-22_7095908865759848448.mp4",
-			"https://ahad-cloud.mirror-index.workers.dev/0:/Uploads/video_2022-05-10_01-11-32_7095909162462006272.mp4",
+			"https://telegra.ph/file/a3bba103ea7daf132a3d6.mp4",
+			"https://telegra.ph/file/a3bba103ea7daf132a3d6.mp4",
+			"https://telegra.ph/file/a3bba103ea7daf132a3d6.mp4",
+			"https://telegra.ph/file/a3bba103ea7daf132a3d6.mp4",
+			"https://telegra.ph/file/a3bba103ea7daf132a3d6.mp4",
+			"https://telegra.ph/file/a3bba103ea7daf132a3d6.mp4",
+			"https://telegra.ph/file/a3bba103ea7daf132a3d6.mp4",
 		];
 		const selected = gifs[Math.floor(Math.random() * gifs.length)];
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
