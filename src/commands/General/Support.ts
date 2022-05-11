451c02bebb007ab594ca644909e84bb3c0896aff
HARAJIT <94275149+AT3NA@users.noreply.github.com>
2022-05-11 17:05:23 +0530

    Update Support.ts

--- src/commands/General/Support.ts
+++ src/commands/General/Support.ts
@@ -19,11 +19,11 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         (await this.client.sendMessage(
         M.sender.jid,
-        `    ♥️ SUPPORT ♥️\n\n*🎇 LELOUCH SUPPORT:🎇* *https://t.me/asunamirror*\n\n*SURPRISE*:*https://t.me/asunamirror*`,
+        `    ♥️ SUPPORT ♥️\n\n*⚡ RAIDEN SUPPORT:⚡* *https://t.me/asunamirror*\n\n*SURPRISE*:*https://t.me/asunamirror*`,
            MessageType.text
         ))
         const n = [
-            'https://ahad-cloud.mirror-index.workers.dev/0:/Uploads/video_2022-05-06_02-35-40_7094446503847251968.mp4'
+            'https://telegra.ph/file/bd74054ac8c3026471bc1.mp4'
         ]
         let beckylynch = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url:beckylynch }, MessageType.video, {quoted:M.WAMessage,
