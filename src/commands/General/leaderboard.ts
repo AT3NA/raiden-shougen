589a7b9ef3122b80f12368138f7f5dc2df07a8a6
HARAJIT <94275149+AT3NA@users.noreply.github.com>
2022-05-27 14:35:53 +0530

    Update leaderboard.ts

--- src/commands/General/leaderboard.ts
+++ src/commands/General/leaderboard.ts
@@ -103,7 +103,7 @@ export default class Command extends BaseCommand {
     }
     return void M.reply(
       await this.client.getBuffer(
-        "https://wallpapermemory.com/uploads/711/chitoge-kirisaki-wallpaper-full-hd-323316.jpg"
+        "https://telegra.ph/file/49acb1548b9e50b43c364.jpg"
       ),
       MessageType.image,
       undefined,
